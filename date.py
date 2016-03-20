5bdd6264b036048a7d9e09c552783d1b135757af
Boris S <boris.segret@obspm.fr>
2016-03-20 17:58:12 +0100

    Commentaires essentiellement

--- date.py
+++ date.py
@@ -17,21 +17,24 @@ class Date:
     -time_integer_part: integer part of mjd time
     -time_decimal_part: decimal part of mjd time in seconds
 
-    Methods defines here:
+    Methods defined here:
     -vts(): return epoch as vts format with MJD type.
 
     """
     def __init__(self):
 	"""Constructor of the class Date."""
-	self.dates = []
+	self.dates = [] 	# pas compris l'interet
+	# ces 2 proprietés pourrait etre ecrites plus courtes?
+	# self.dayvts & slef.secvts (pour "day" et "seconds" au format VTS)
 	self.time_integer_part = 0
-	self.time_decimal_part = 0
+	self.time_decimal_part = 0 # ceci est-il bien un reel?
     def __repr__(self):
 	"""Method displaying a customized message when an instance of 
 	the class Date is called in the command line.
 	"""
 	return "Date: this is the epoch ({})".format(self.dates)
     def vts(self, dates):
+	# pas compris, si un objet est de type "Date", sa méthode t.vts(...) utilise quoi et retourne quoi?
 	"""Method converting an epoch to the vts format.
 	it returns the integer part of the MJD and
 	the fractional part (converted to seconds) of the MJD. 
